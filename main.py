1d505835a24302a43cdbe8aa3a6c56e94e0a0d83
Fsoi <fransouze@gmail.com>
2020-05-10 13:19:13 +0200

    apply black formatting to main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import sys
 import inspect
 from PyQt5.QtWidgets import *
 
-sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
 
 from nodeeditor.utils import loadStylesheet
 from nodeeditor.node_editor_window import NodeEditorWindow
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     wnd = NodeEditorWindow()
     wnd.tabs_project.model_tab.nodeEditor.addNodes()
-    module_path = os.path.dirname( inspect.getfile(wnd.__class__) )
+    module_path = os.path.dirname(inspect.getfile(wnd.__class__))
 
-    loadStylesheet( os.path.join( module_path, 'qss/nodestyle.qss') )
+    loadStylesheet(os.path.join(module_path, "qss/nodestyle.qss"))
 
     sys.exit(app.exec_())
